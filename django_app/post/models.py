64f6911abf424e69a1439b906b2adc6a0b50abc9
hanabee337 <hanabee337@gmail.com>
2017-03-15 09:49:12 +0900

    InstagramAPI - to_dict 메서드 구현(part 1)
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- django_app/post/models.py
+++ django_app/post/models.py
@@ -7,6 +7,17 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL)
     created_date = models.DateTimeField(auto_now_add=True)
 
+    def to_dict(self):
+        """
+        post 객체 한 개에 대해서 인스턴스해서 dict형태로 바꿔주는 메서드
+        """
+        ret = {
+            'pk': self.pk,
+            'created_date': self.created_date,
+            'author': self.author.pk,
+        }
+        return ret
+
 
 class PostPhoto(models.Model):
     post = models.ForeignKey(Post)
