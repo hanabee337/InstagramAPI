91323e2fdfb1b3d5c3e25873c3d16287c3d759fa
hanabee337 <hanabee337@gmail.com>
2017-03-18 14:16:38 +0900

    InstagramAPI - test_cannot_post_create_not_authenticated테스트 작성 완료(2017.03.16) app_name을 안쓰는 이유: DRF를 쓴다는 가정하에선 namespace를 쓰는 것이 엄청 복잡하기 때문. (바로 지난 시간 경험)
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- django_app/post/tests/tests_apis.py
+++ django_app/post/tests/tests_apis.py
@@ -1,8 +1,9 @@
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework import status
-
 from rest_framework.test import APILiveServerTestCase
+
+from post.models import Post
 
 User = get_user_model()
 
@@ -26,6 +27,7 @@ class PostTest(APILiveServerTestCase):
         print('url: {}'.format(url))
         response = self.client.post(url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(Post.objects.exists(), False)
 
     def test_post_list(self):
         pass
--- django_app/post/urls.py
+++ django_app/post/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
-app_name = 'post'
+# app_name = 'post'
 urlpatterns = [
-    url(r'^create/$', views.post_create, name='create'),
+    url(r'^create/$', views.post_create, name='post-create'),
     url(r'^photo/add/$', views.post_photo_add, name='photo_add'),
     url(r'^list/$', views.post_list, name='list'),
 ]
