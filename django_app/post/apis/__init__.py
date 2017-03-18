84e1c1b6eb52b079bbcec7086c6ea7c34d877f42
hanabee337 <hanabee337@gmail.com>
2017-03-18 21:34:11 +0900

    InstagramAPI - PostList permission 지정 (2017.03.16) Expected Result : Still Fail since commit : ca454ff8173a380329663f841a62042e339914b6
    FAIL: test_post_create (post.tests.tests_apis.PostTest)
    ----------------------------------------------------------------------
    Traceback (most recent call last):
      File "/home/hanabee2/projects/wps-4th/instagram_api/django_app/post/tests/tests_apis.py", line 47, in test_post_create
        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
    AssertionError: 403 != 201
    
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- django_app/post/apis/__init__.py
+++ django_app/post/apis/__init__.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework import permissions
 
 from post.models import Post
 from serializers import PostSerializer
@@ -7,6 +8,8 @@ from serializers import PostSerializer
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
