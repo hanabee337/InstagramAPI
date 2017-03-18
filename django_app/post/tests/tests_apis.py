712c3b9860def0cb6eec8ed50f4052c8fa2b11cd
hanabee337 <hanabee337@gmail.com>
2017-03-18 22:48:53 +0900

    InstagramAPI - test_post_list 테스트 코드 구현(2017.03.16) 'create_user_and_login' helper-func is made as well. Expected Result : OK
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- django_app/post/tests/tests_apis.py
+++ django_app/post/tests/tests_apis.py
@@ -22,6 +22,13 @@ class PostTest(APILiveServerTestCase):
         )
         return user
 
+    def create_user_and_login(self):
+        self.create_user()
+        self.client.login(
+            username=self.test_username,
+            password=self.test_password,
+        )
+
     def create_post(self, num=1):
         """
         :param num: 생성할 Post수
@@ -54,6 +61,7 @@ class PostTest(APILiveServerTestCase):
             password=self.test_password,
         )
 
+        # Post 생성
         response = self.create_post()
 
         # response의 status_code가 201(Created)이어야 함
@@ -73,18 +81,26 @@ class PostTest(APILiveServerTestCase):
 
     def test_post_list(self):
         # Post 생성 위해 유저 생성 후 로그인
+        self.create_user_and_login()
 
         # 생성할 Post 개수 지정
+        num = random.randrange(1, 50)
 
         # num만큼 Post 생성
+        self.create_post(num)
 
         # Post를 생성하는 API주소를 reverse
+        url = reverse('api:post-list')
 
-        # Post를 생성하는 API주소에 POST요청, response를 받아옴
+        # Post를 생성하는 API주소에 GET요청, response를 받아옴
+        response = self.client.get(url)
+        # print(response.data)
 
         # status_code 확인
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         # num만큼 생성되었는지 확인
+        self.assertEqual(len(response.data), num)
 
     def test_post_update(self):
         pass
@@ -92,10 +108,8 @@ class PostTest(APILiveServerTestCase):
     def test_post_partial_update(self):
         pass
 
+    def test_post_retrieve(self):
+        pass
 
-def test_post_retrieve(self):
-    pass
-
-
-def test_post_destroy(self):
-    pass
+    def test_post_destroy(self):
+        pass
