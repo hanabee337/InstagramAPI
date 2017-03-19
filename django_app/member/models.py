7cfcf4dfbdf1de79f103146662e86f5e135f61e5
hanabee337 <hanabee337@gmail.com>
2017-03-19 21:15:32 +0900

    InstagramAPI - MyUser의 relation필드 추가 및 IntermediateModel Relationship추가, migration생성(2017.03.17) - part 1
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- django_app/member/models.py
+++ django_app/member/models.py
@@ -3,11 +3,27 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class MyUser(AbstractUser):
-    def to_dict(self):
-        ret = {
-            'pk': self.pk,
-            'username': self.username,
-            'first_name': self.first_name,
-            'last_name': self.last_name,
-        }
-        return ret
+    """
+    1. 팔로우, 차단을 함께 만들 수 있는 중간자모델을 구현
+        검색해보세요!
+        django follower twitter model
+            class User
+                relation
+                    through='Relation'
+
+            class Relationship:
+                from
+                to
+                type CharField(choices=(follow, block))
+
+    2. MyUser의
+        method:
+            follow: 내가 "누군가를" follow하기
+            block: 내가 "누군가를" block하기
+        property:
+            friends: 서로 follow하고 있는 관계
+            followers: 나를 follow하고 있는 User들
+            following: 내가 follow하고 있는 User들
+            block_users: 내가 block한 User들
+    """
+    pass
