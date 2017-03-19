5fed07550bee9e7c24634f6a98de760c5ef1b96f
hanabee337 <hanabee337@gmail.com>
2017-03-19 19:06:36 +0900

    InstagramAPI - PostSerializer에 postphoto_set필드 추가 및 post_create, post_list test에서 해당내용 추가(2017.03.17) - part 1
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- django_app/post/serializers/post.py
+++ django_app/post/serializers/post.py
@@ -10,7 +10,7 @@ __all__ = (
 
 class PostSerializer(serializers.ModelSerializer):
     """
-    Mission - 2017.03.17
+    Mission 1. - 2017.03.17
 
         author필드의 값이 pk가 아닌, 하나의 Object(dict)로 나타나도록 수정
         http://www.django-rest-framework.org/api-guide/serializers/#dealing-with-nested-objects
@@ -18,6 +18,14 @@ class PostSerializer(serializers.ModelSerializer):
     1. 관련 테스트코드 작성
         post_list, post_create부분
     2. 해당 테스트코드가 작동하도록 PostSerializer를 수정 및 UserSerializer작성
+    3. 테스트 및 postman 작동 확인
+
+    Mission 2. - 2017.03.17
+        Post에 연결된 PostPhoto를 List 내부의 Object 형태로 리턴
+
+    1. 관련 테스트코드 작성
+        post_list, post_create부분
+    2. 해당 테스트코드가 작동하도록 PostSerializer 를 수정 및 PostPhotoSerializer 작성
     3. 테스트 및 postman 작동 확인
     """
 
