dfd24f36d790c5587e9b7eb3c6a81c55eba1d2ef
hanabee337 <hanabee337@gmail.com>
2017-03-20 21:56:02 +0900

    InstagramCBV - Class-Based View 작성하기 - part 2
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- django_app/post/views/post.py
+++ django_app/post/views/post.py
@@ -5,3 +5,20 @@ Mission 1.
         PostList, PostDetail, PostCreate, PostDelete view를 작성
         (내용 없음)
 """
+from django.views import View
+
+
+# funcion-based view였다면, 아래와 같았을 것이다.
+# def post_list(request):
+#     pass
+
+class PostList(View):
+    pass
+
+
+class PostDetail(View):
+    pass
+
+
+class PostDelete(View):
+    pass
