7b1186f913e179cc96cb03a7067f5750d91d5dd0
hanabee337 <hanabee337@gmail.com>
2017-03-20 22:57:18 +0900

    InstagramCBV - settings에 STATIC_DIR관련내용 추가
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -17,11 +17,22 @@ DEBUG = os.environ.get('MODE') == 'DEBUG'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ROOT_DIR = os.path.dirname(BASE_DIR)
+
+# Templates
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
+# Media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+# Static
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+STATIC_URL = '/static/'
+
+# Config files
 CONF_DIR = os.path.join(ROOT_DIR, '.conf-secret')
 CONFIG_FILE_COMMON = os.path.join(CONF_DIR, 'settings_common.json')
 if DEBUG:
@@ -143,4 +154,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+
