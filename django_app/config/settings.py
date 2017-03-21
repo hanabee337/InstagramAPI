63ba87a6e490572af5e9fdfa6171836c0c0e83fa
hanabee337 <hanabee337@gmail.com>
2017-03-21 18:51:20 +0900

    InstagramAjax - CORS설정
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -77,9 +77,12 @@ INSTALLED_APPS = [
 
     'member',
     'post',
+
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -88,6 +91,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:8080',
+)
 
 ROOT_URLCONF = 'config.urls'
 
