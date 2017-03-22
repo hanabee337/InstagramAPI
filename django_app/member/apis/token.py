73e3c7bc73119cd72dee46e7e260db2e8ce239bb
hanabee337 <hanabee337@gmail.com>
2017-03-22 17:35:58 +0900

    InstagramAPI - ProfileView구현
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

---
+++ django_app/member/apis/token.py
@@ -0,0 +1,12 @@
+from rest_framework.views import APIView
+
+__all__ = (
+    'DeleteToken',
+)
+
+
+class DeleteToken(APIView):
+    """
+    POST 요청이 오면,
+    """
+    pass
--- django_app/member/urls/apis.py
+++ django_app/member/urls/apis.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
-from rest_framework.authtoken import views
+from rest_framework.authtoken import views as auth_token_views
+
+from .. import apis
 
 urlpatterns = [
-    url(r'^token-auth/', views.obtain_auth_token),
+    url(r'^token-auth/', auth_token_views.obtain_auth_token),
+    url(r'^profile/', apis.ProfileView.as_view()),
 ]
