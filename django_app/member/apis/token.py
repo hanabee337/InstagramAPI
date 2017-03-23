4cd19a3eec71fb1713d9eae20ed2fc2dfc9c987c
hanabee337 <hanabee337@gmail.com>
2017-03-23 15:06:11 +0900

    InstagramAPI - DeleteToken view구현
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- django_app/member/apis/token.py
+++ django_app/member/apis/token.py
@@ -1,3 +1,6 @@
+from rest_framework import permissions
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 __all__ = (
@@ -7,6 +10,16 @@ __all__ = (
 
 class DeleteToken(APIView):
     """
-    POST 요청이 오면,
+    2017.03.22
+    H/W
+    POST 요청이 오면, request.user가 인증되어 있는 경우, request.auth의 Token을 삭제
     """
-    pass
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, format=None):
+        # print(request.auth)
+        # print(type(request.auth))
+        # cur_token = Token.objects.get(ket=request.auth.key)
+        # print(cur_token)
+        request.auth.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- django_app/member/urls/apis.py
+++ django_app/member/urls/apis.py
@@ -6,4 +6,6 @@ from .. import apis
 urlpatterns = [
     url(r'^token-auth/', auth_token_views.obtain_auth_token),
     url(r'^profile/', apis.ProfileView.as_view()),
+
+    url(r'^token-delete/$', apis.DeleteToken.as_view()),
 ]
